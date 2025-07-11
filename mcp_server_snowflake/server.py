b5fb6b81092cec98c9b5307e0a877942d4c263fd
Jason Summer <jason.summer@snowflake.com>
2025-07-11 10:45:59 -0500

    Lock after version bump

--- mcp_server_snowflake/server.py
+++ mcp_server_snowflake/server.py
@@ -35,7 +35,7 @@ from mcp_server_snowflake.utils import (
 # Used to quantify Snowflake usage
 server_name = "mcp-server-snowflake"
 tag_major_version = 0
-tag_minor_version = 3
+tag_minor_version = 4
 query_tag = {"origin": "sf_sit", "name": "mcp_server"}
 
 logger = logging.getLogger(server_name)
