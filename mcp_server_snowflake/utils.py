e460c3bf5e6f85eae956e5c295b217a86bd9c7fa
Jason Summer <jason.summer@snowflake.com>
2025-08-07 15:08:12 -0500

    Remove commented line

--- mcp_server_snowflake/utils.py
+++ mcp_server_snowflake/utils.py
@@ -135,7 +135,6 @@ class SnowflakeResponse:
             If connection fails or SQL execution encounters an error
         """
         # Forward any remaining kwargs to get_connection
-        # with service.get_connection(use_dict_cursor=True, **kwargs) as (
         with service.get_connection(
             use_dict_cursor=True, session_parameters=service.get_query_tag_param()
         ) as (
